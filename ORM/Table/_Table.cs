4f697e25c20b49282ad9e81136c9dba848ab186a
linbaozhong <lin.baozhong@hotmail.com>
2018-07-24 13:10:45 +0800

    111

--- ORM/Table/_Table.cs
+++ ORM/Table/_Table.cs
@@ -47,10 +47,8 @@ namespace Snow.Orm
             if (SelectColumns.TryGetValue(_key, out _selectColumns)) return _selectColumns;
 
             var _strings = new List<string>();
-            //var _index = -1;
             foreach (var arg in args)
             {
-                //_index = _Columns.IndexOf(arg);
                 if (_TColumns.Contains(arg))
                 {
                     _strings.Add(DB.GetName(arg));   // 聚合函数
